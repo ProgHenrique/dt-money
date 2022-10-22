cefef7c391991ed74ade2e942a25329648085c25
ProgHenrique <henrique123187@gmail.com>
2022-10-22 01:20:02 -0300

    dt-money website

--- server/prisma/migrations/20221019010107_transactions/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "transactions" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "description" TEXT NOT NULL,
-    "price" INTEGER NOT NULL,
-    "category" TEXT NOT NULL,
-    "type" TEXT NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
